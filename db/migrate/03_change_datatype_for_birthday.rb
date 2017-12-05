7d9151580cd58d3d814082a6a5c82a319df28b53
Lara Sanders <larasanders@gmail.com>
2017-12-05 18:57:03 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthday.rb
+++ db/migrate/03_change_datatype_for_birthday.rb
@@ -1,4 +1,4 @@
-class ChangeDatatypeForBirthdate < ActiveRecord::Migration
+class ChangeDatatypeForBirthday < ActiveRecord::Migration
   def change
     change_column :students, :birthdate, :datetime
   end
